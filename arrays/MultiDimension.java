e0a337635a1ab998c2e07e7b1a5c72bb5e39f996
KABANOFESTO <kabanofesto1@gmail.com>
2026-01-28 15:03:32 +0200

    ch:test function with new method

--- arrays/MultiDimension.java
+++ arrays/MultiDimension.java
@@ -1,7 +1,7 @@
 package arrays;
 
 public class MultiDimension {
-    public static void main(String[] args) {
+    public MultiDimension() {
         int[][] matrix = {
                 { 1, 2, 3 },
                 { 4, 5, 6 },
@@ -13,5 +13,9 @@ public class MultiDimension {
                 System.out.println(matrix[i][j]);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        new MultiDimension();
     }
 }

dd074cdcf7391943aaeff3b4894d5bf6e347e4c1
KABANOFESTO <kabanofesto1@gmail.com>
2026-02-17 22:35:59 +0200

    ch:addTwoNumber

---
+++ LeetCode/addTwoNumber.java
@@ -0,0 +1,67 @@
+package LeetCode;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class addTwoNumber {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static ListNode buildList(int[] nums) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        for (int num : nums) {
+            curr.next = new ListNode(num);
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) System.out.print(" -> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        addTwoNumber solution = new addTwoNumber();
+
+        ListNode l1 = buildList(new int[]{2, 4, 3});
+        ListNode l2 = buildList(new int[]{5, 6, 4});
+
+        ListNode result = solution.addTwoNumbers(l1, l2);
+        printList(result); 
+    }
+}

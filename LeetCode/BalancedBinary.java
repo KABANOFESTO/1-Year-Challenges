27aee9f4bb9c823e489f684bf46103be1afab78e
KABANOFESTO <kabanofesto1@gmail.com>
2026-02-19 20:50:37 +0200

    ch:add balance Binary tree

---
+++ LeetCode/BalancedBinary.java
@@ -0,0 +1,56 @@
+package LeetCode;
+
+public class BalancedBinary {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    private int checkHeight(TreeNode node) {
+        if (node == null)
+            return 0;
+
+        int left = checkHeight(node.left);
+        if (left == -1)
+            return -1;
+
+        int right = checkHeight(node.right);
+        if (right == -1)
+            return -1;
+
+        if (Math.abs(left - right) > 1)
+            return -1;
+
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        BalancedBinary solution = new BalancedBinary();
+
+        TreeNode root1 = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20,
+                        new TreeNode(15),
+                        new TreeNode(7)));
+        System.out.println("test: " + solution.isBalanced(root1));
+    }
+}
